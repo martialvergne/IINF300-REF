e694a6d9b44c2c7f0bed5fddbe54d6838490355a
martialvergne <martial.vergne@gmail.com>
2025-02-28 15:19:48 +0100

    Add helm

--- docker/app/main.py
+++ docker/app/main.py
@@ -7,14 +7,6 @@ fnac.ajouter_livre(["La guerre des mondes", "HG Wells"])
 app = Flask(__name__)
 
 
-def home():
-    return "Hello, Docker!"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-
 @app.route("/test")
 def hello_world():
     return "Ma page html est ici"
